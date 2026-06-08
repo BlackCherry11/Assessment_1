590ad10f8a139a8e4820559b317700f2f7c21f75
Sai Raghavendra Kunchala <srkunchala123@gmail.com>
2026-06-08 16:11:43 +0300

    Update P3.py

---
+++ P3.py
@@ -0,0 +1,16 @@
+s = input().strip()
+
+last_seen = {}
+left = 0
+max_len = 0
+
+for right in range(len(s)):
+    ch = s[right]
+
+    if ch in last_seen and last_seen[ch] >= left:
+        left = last_seen[ch] + 1
+
+    last_seen[ch] = right
+    max_len = max(max_len, right - left + 1)
+
+print(max_len)
